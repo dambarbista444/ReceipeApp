01cb6e9cc7f07d84389f9beaffa15512624fac5f
Dambar B. Bista <dambarbista.db@gmail.com>
2024-08-24 12:43:03 -0400

    Initial Commit

---
+++ FetchReceipeApp/FetchReceipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchReceipeAppApp.swift
+//  FetchReceipeApp
+//
+//  Created by Dambar Bista on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchReceipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
